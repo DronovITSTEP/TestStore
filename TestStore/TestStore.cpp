269ce115e70a6f7b05a368491c00728b28d56458
ApxNord <dronov_a@t.top-academy.ru>
2024-06-17 18:44:54 +0300

    правки в main

--- TestStore/TestStore.cpp
+++ TestStore/TestStore.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
 #include "Employ.h"
+#include "Seller.h"
+#include "Storekeeper.h"
+#include "Cashier.h"
 #include "Customer.h"
 #include "WareHouse.h"
 #include "Tovar.h"
@@ -14,24 +17,24 @@ int main()
 
     Customer customer{ "Ivan", "Ivanov", "ivan@ivanov.ru", "899997777", 12400 };
     WareHouse stock;
-    Employ* empl[3];
-    empl[0] = new Seller("Petr", "Petrov", "Saler", 47555 );
-    empl[1] = new Storekeeper("Sidr", "Sidorov", "Storekeeper", 36500 );
-    empl[2] = new Cashier("Elena", "Elenova", 60000 );
+    Employ* empl[3]{
+        new Seller("Petr", "Petrov", "Saler", 47555),
+    new Storekeeper("Sidr", "Sidorov", "Storekeeper", 36500),
+    new Cashier("Elena", "Elenova","Dou", 60000)
     };
 
     _File file;
     file.Open("Products.txt");
     Product* products = file.GetProducts();
     for (int i = 0; i < 10; i++) {
-        stock.AddProduct(products[i]->name, products[i]->price, products[i]->count);
+        stock.AddProduct(products[i].name, products[i].price, products[i].count);
     }
 
     customer.AddToBasket(empl[SELLER]->GetProduct("молоко", 2));
     customer.AddToBasket(empl[SELLER]->GetProduct("хлеб", 1));
     customer.AddToBasket(empl[SELLER]->GetProduct("яйца", 4));
 
-    float totalPrice = empl[CASHIER].GetTotalPrice(customer.GetBasket(), 
+    float totalPrice = empl[CASHIER]->GetTotalPrice(customer.GetBusket(),
         customer.GetDiscount());
     if (empl[CASHIER].Pay(customer.GetCash(totalPrice))) {
         cout << "Ok";
